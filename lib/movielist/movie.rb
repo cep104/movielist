e256afdc4bb3ba068745d653c4eacd5e15aa3206
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-08 22:17:13 +0000

    updated movie class

--- lib/movielist/movie.rb
+++ lib/movielist/movie.rb
@@ -1,4 +1,5 @@
 class Movielist::Movie 
+  attr_accessor :name, :total, :weeks, :url
   def self.boxoffice 
    puts <<-DOC 
 01. Movie 01, box office total, weeks in theater
@@ -13,10 +14,17 @@ class Movielist::Movie
 10. Movie 10, box office total, weeks in theater 
 
  DOC
-# movie_1 = Movie.new 
-# movie_1.name = "Movie 01"
-# movie_1.total = "box office total"
-# movie_1.weeks = "weeks in theater"
-# movie_1.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+movie_1 = Movie.new 
+movie_1.name = "Movie 01"
+movie_1.total = "box office total"
+movie_1.weeks = "weeks in theater"
+movie_1.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_2 = Movie.new 
+movie_2.name = "Movie 02"
+movie_2.total = "box office total"
+movie_2.weeks = "weeks in theater"
+movie_2.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+[movie_1, movie_2]
   end
 end 
