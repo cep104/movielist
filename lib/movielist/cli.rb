7e7ed7d1cffa4c37e04800176431b4ef504ed4d8
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-09 18:46:20 +0000

    movies added

--- lib/movielist/cli.rb
+++ lib/movielist/cli.rb
@@ -9,6 +9,9 @@ class Movielist::CLI
   def list_movies
   puts " This weeks box office results:"
  @movie = Movielist::Movie.boxoffice
+    @movie.each.each.with_index(1) do |movies, index| 
+      puts "#{index}. #{movies}"
+    end
   end
   
   def menu 
@@ -16,31 +19,12 @@ class Movielist::CLI
     while input != "exit"
     puts "Enter the number of the movie you would like to learn more about, type list to see movies again or type exit"
       input =gets.chomp.downcase
-      case input 
-      when "1"
-        puts "info on movie 1"
-      when "2"
-        puts "info on movie 2"
-      when "3"
-        puts "info on movie 3"
-      when "4"
-        puts "info on movie 4"
-      when "5"
-        puts "info on movie 5"
-      when "6"
-        puts "info on movie 6"
-      when "7"
-        puts "info on movie 7"
-      when "8"
-        puts "info on movie 8"
-      when "9"
-        puts "info on movie 9"
-      when "10"
-        puts "info on movie 10"
-      when "list"
+      if input.to_i > 0 
+        puts @movie[input.to_i-1]
+      elsif input == "list"
         list_movies
-      else 
-        puts "Not sure what you want type list or exit"
+        else
+          puts "Not sure what you want type list or exit"
       end
     end
   end
--- lib/movielist/movie.rb
+++ lib/movielist/movie.rb
@@ -27,6 +27,54 @@ movie_2.total = "box office total 2"
 movie_2.weeks = "weeks in theater 2"
 movie_2.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
 
-[movie_1, movie_2]
+movie_3 = self.new 
+movie_3.name = "Movie 03"
+movie_3.total = "box office total 2"
+movie_3.weeks = "weeks in theater 2"
+movie_3.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_4 = self.new 
+movie_4.name = "Movie 04"
+movie_4.total = "box office total 2"
+movie_4.weeks = "weeks in theater 2"
+movie_4.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_5 = self.new 
+movie_5.name = "Movie 05"
+movie_5.total = "box office total 2"
+movie_5.weeks = "weeks in theater 2"
+movie_5.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_6 = self.new 
+movie_6.name = "Movie 06"
+movie_6.total = "box office total 2"
+movie_6.weeks = "weeks in theater 2"
+movie_6.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_7 = self.new 
+movie_7.name = "Movie 07"
+movie_7.total = "box office total 2"
+movie_7.weeks = "weeks in theater 2"
+movie_7.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_8 = self.new 
+movie_8.name = "Movie 08"
+movie_8.total = "box office total 2"
+movie_8.weeks = "weeks in theater 2"
+movie_8.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_9 = self.new 
+movie_9.name = "Movie 09"
+movie_9.total = "box office total 2"
+movie_9.weeks = "weeks in theater 2"
+movie_9.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+movie_10 = self.new 
+movie_10.name = "Movie 10"
+movie_10.total = "box office total 2"
+movie_10.weeks = "weeks in theater 2"
+movie_10.url = "https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.htm"
+
+[movie_1, movie_2, movie_3, movie_4, movie_5, movie_6, movie_7, movie_8, movie_9, movie_10]
   end
 end 
