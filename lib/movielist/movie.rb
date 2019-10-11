645fe4ab384c581337377f36ee0085d7de9f87d4
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-11 05:28:06 +0000

    second movie part 1 done

--- lib/movielist/movie.rb
+++ lib/movielist/movie.rb
@@ -9,6 +9,7 @@ class Movielist::Movie
   def self.scrape_movies
     movies = []
     movies << self.scrape_joker
+    movies << self.scrape_movietwo
     
   end
   
@@ -29,13 +30,20 @@ class Movielist::Movie
     # binding.pry
   end
   
-  # def self.scrape_tomato_joker
-  #   doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/joker_2019"))
-  #   selector = doc.search("span.mop-ratings-wrap__percentage").text.split(" ")
-  #   rm_score = selector[0]
-  #   audience_score = selector[1]
-  #   synopsis = doc.search("div#movieSynopsis").text.strip
-  #   binding.pry
-  # end
- 
+  def self.scrape_movietwo 
+    doc = Nokogiri::HTML(open("https://www.boxofficemojo.com/movies/?id=everest2019.htm"))
+    movie = self.new
+    selector = "table td b"
+    anchors = doc.css(selector)
+    movie.name = anchors.map{|a| a.text}[1].chomp(" (2019)")
+    movie.total = anchors.map{|a| a.text}[2]
+    movie.studio = anchors.map{|a| a.text}[3]
+    # doc_rotten = Nokogiri::HTML(open("https://www.rottentomatoes.com/m/joker_2019"))
+    # rotten = doc_rotten.search("span.mop-ratings-wrap__percentage").text.split(" ")
+    # movie.rm_score = rotten[0]
+    # movie.audience_score = rotten[1]
+    # movie.synopsis = doc_rotten.search("div#movieSynopsis").text.strip
+    movie
+    binding.pry
+ end
 end 
