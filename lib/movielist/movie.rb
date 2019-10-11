5e2cff6da659c750fdc8efebf637a66785f39528
Candice Peters <candicelizabethpeters@gmail.com>
2019-10-11 01:43:26 +0000

    fixed scrapers

--- lib/movielist/movie.rb
+++ lib/movielist/movie.rb
@@ -11,31 +11,15 @@ class Movielist::Movie
     movies << self.scrape_joker
   end
   
-  # def self.scrape_mojo
-  #   doc = Nokogiri::HTML(open("https://www.boxofficemojo.com/weekend/chart/?view=main&yr=2019&wknd=40&p=.html"))
-  #   movie = self.new
-  #   selector = "tr td a"
-  #   anchors = doc.css(selector)
-    
-  #   movie_titles = anchors.map{|a| a.text}
-  #   movie_one_name = movie_titles[17].split.first
-  #   movie_two_name = movie_titles[19]
-  #   list = doc.css("tr td b").text.split
-  #   movie_one_total = list[11].chomp("Abominable$11,921,855Downton").gsub("(2019)","").strip
-  #   movie_two_total = list[11].chomp("Downton").gsub("(2019)$96,202,337Abominable","").strip
-  #   movie
-    
-  # end
-  
   def self.scrape_joker
     doc = Nokogiri::HTML(open("https://www.boxofficemojo.com/movies/?id=joker2019.htm"))
     movie = self.new
     selector = "table td b"
     anchors = doc.css(selector)
-    self.name = anchors.map{|a| a.text}[1].chomp(" (2019)")
-    self.total = anchors.map{|a| a.text}[2]
-    self.studio = anchors.map{|a| a.text}[3]
+    movie.name = anchors.map{|a| a.text}[1].chomp(" (2019)")
+    movie.total = anchors.map{|a| a.text}[2]
+    movie.studio = anchors.map{|a| a.text}[3]
     movie
-    binding.pry
+    # binding.pry
   end
 end 
